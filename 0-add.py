4702c597fabc4102c87802757d89156155f3fc6a
nawalsamer04 <nawalsamer04@gmail.com>
2026-03-07 06:59:34 +0000

    Initialize python_variable_annotations and add task 0

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module containing a type-annotated add function."""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers."""
+    return a + b
---
+++ README.md
@@ -0,0 +1,3 @@
+# Python - Variable Annotations
+
+This project practices Python 3 type annotations for functions and variables.
